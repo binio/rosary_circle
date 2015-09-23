cf088233db79a3ada715fc317cf763530b5e694b
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2015-09-23 11:30:08 +0100

    add dynamic months and all intention tab

--- rosecircle.js
+++ rosecircle.js
@@ -1,17 +1,25 @@
 IntencjeCollection = new Mongo.Collection("intencje");
 
 if (Meteor.isClient) {
+  moment.locale('pl');
   // counter starts at 0
     Template.intentionList.helpers({
-        'intentionList':function(){
-        return IntencjeCollection.find();
+        'intentionListCurrentMonth':function(){
+        var currentMonth = moment().endOf('month').month();
+        return IntencjeCollection.find({month:currentMonth});
+        },
+        'intentionListNextMonth':function(){
+        var nextMonth = moment().add(1, 'months').endOf('month').month();
+        return IntencjeCollection.find({month:nextMonth});
         },
 
         'currentMonth':function(){
-            return "September";
+
+            return moment().endOf('month').format('MMMM');
         },
         'nextMonth':function(){
-            return "October";
+
+            return moment().add(1, 'months').endOf('month').format('MMMM');
         }
     });
     
@@ -38,12 +46,15 @@ if (Meteor.isClient) {
     Template.intentionList.events({
         'click .currentMonth':function(){
             $('.currentMonthPanel').show();
+            $('.nextMonthPanel').hide();
             $('a.nextMonth').parent().removeClass('active');
             $('a.currentMonth').parent().addClass('active');
             console.log("Current Month");
         },
         'click .nextMonth':function(){
             $('.currentMonthPanel').hide();
+            $('.nextMonthPanel').show();
+            $('.nextMonthPanel').removeClass('hidden');
             $('a.nextMonth').parent().addClass('active');
             $('a.currentMonth').parent().removeClass('active');
             console.log("Next Month");
@@ -59,5 +70,6 @@ if (Meteor.isServer) {
     IntencjeCollection.insert({name:"Moja pierwsza intencja", month:9, year:2015});
     IntencjeCollection.insert({name:"Moja druga intencja", month:9, year:2015});
     IntencjeCollection.insert({name:"Moja trzecia intencja", month:9, year:2015});
+    IntencjeCollection.insert({name:"Moja trzecia intencja 8", month:8, year:2015});
   });
 }
