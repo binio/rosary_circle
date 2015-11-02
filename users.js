86b7aeb344332cb9f08b689d079b6dc5a68dd0cd
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2015-11-02 16:39:03 +0000

    edit users

--- users.js
+++ users.js
@@ -1,7 +1,20 @@
+UsersCollection = Meteor.users;
+
 if (Meteor.isClient) {
-     Template.users.helpers({});
+
+    Meteor.subscribe('users');
+
+     Template.me.helpers({
+         'allUsers':function(){
+            return UsersCollection.find({});
+         }
+     });
 }
 
 if (Meteor.isServer) {
+
+    Meteor.publish('users', function(){
+            return Meteor.users.find({});
+        });
 
 }
