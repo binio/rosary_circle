bffe4ad11c75a2bff7e25b0b7a44353d45ae7609
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2015-11-10 16:28:34 +0000

    edit permissions

--- user.js
+++ user.js
@@ -1,2 +1,20 @@
-if (Meteor.isClient) {}
-if (Meteor.isServer) {}
+if (Meteor.isClient) {
+    Template.userDetail.events({
+        'click .admin':function(){
+            //@TODO First check if current user is admin
+            Meteor.call('addAdmin',this._id);
+            console.log(this._id);
+        }
+    });
+}
+if (Meteor.isServer) {
+    Meteor.methods({
+            'addAdmin':function(id){
+                var loggedInUser = Meteor.user();
+                if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['admin', 'user'], 'group-1')) {
+      throw new Meteor.Error(403, "Access denied")
+    }
+                Roles.addUsersToRoles(id, ['user','admin'], 'group-1');
+            }
+    });
+}
