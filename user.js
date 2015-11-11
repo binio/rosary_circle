c4002d1ada562652c1ee7676c05c3f05555db4c9
Thomas <tbiniecki@gmail.com>
2015-11-11 23:01:37 +0000

    checkboxes

--- user.js
+++ user.js
@@ -3,19 +3,19 @@ if (Meteor.isClient) {
     Template.userDetail.events({
         'click .admin':function(){
             //@TODO First check if current user is admin
-            Meteor.call('addAdmin',this._id, ['admin'], 'group-1');
+            Meteor.call('addRole',this._id, ['admin'], 'group-1');
             if(Roles.userIsInRole(this._id, ['admin'], 'group-1')){
             console.log('has admin');
             }
         },
         'click .user':function(){
-            Meteor.call('addAdmin',this._id, ['user'], 'group-1');
+            Meteor.call('addRole',this._id, ['user'], 'group-1');
             if(Roles.userIsInRole(this._id, ['user'], 'group-1')){
             console.log('has user');
             }
         },
         'click .priest':function(){
-            Meteor.call('addAdmin',this._id, ['priest'], 'group-1');
+            Meteor.call('addRole',this._id, ['priest'], 'group-1');
             if(Roles.userIsInRole(this._id, ['priest'], 'group-1')){
             console.log('has priest');
             }
@@ -38,10 +38,29 @@ if (Meteor.isClient) {
 
         }
     });
+
+    Template.userDetail.helpers({
+        'checks':function(){
+            var roles = Session.get('userRoles');
+            console.log(roles);
+            var checks = new Object;
+
+            if(roles.indexOf('admin')>=0){
+                checks.adminCheck = 'checked';
+            }
+            if(roles.indexOf('user')>=0){
+                checks.userCheck = 'checked';
+            }
+            if(roles.indexOf('priest')>=0){
+                checks.priestCheck = 'checked';
+            }
+            return checks;
+        }
+    });
 }
 if (Meteor.isServer) {
     Meteor.methods({
-     'addAdmin':function(id, roles, group){
+     'addRole':function(id, roles, group){
         var loggedInUser = Meteor.user();
             if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['admin'], 'group-1')) {
                 throw new Meteor.Error(403, "Access denied")
