e4dab94c03e62c35b8a705ebe07f3bd367d6002e
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2016-04-11 17:21:21 +0100

    add new route for userAssignment

--- router/router.js
+++ router/router.js
@@ -4,6 +4,7 @@ Router.route('/about', { controller: 'AboutController' });
 Router.route('/most', { controller: 'MostController' });
 Router.route('/users', { controller: 'UsersController' });
 Router.route('/bless', { controller: 'BlessController' });
+Router.route('/assignments',{controller:'AssignmentController'});
 Router.route('/user/:_id', function(){
     profile = UsersCollection.findOne({_id: this.params._id});
     this.render('userDetail', {
