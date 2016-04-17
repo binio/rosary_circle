eace3965692b629807dbb3cade213c580ffbbeee
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2016-04-17 21:27:07 +0100

    coment out

--- assignments.js
+++ assignments.js
@@ -1,33 +1,33 @@
 //User assignments for users so they can see what they have been assigned for curent month.
 
 if (Meteor.isClient) {
-    Meteor.subscribe('userAssignment');
-    Session.set('userAssignment',[]);
-
-    Template.assignment.helpers({
-        'userAssignment':function(){
-            Meteor.call('getAssignment',Meteor.userId(),MyApp.currentMonth(), function(error,result){
-                Session.set('userAssignment',result);
-            });
-            return Session.get('userAssignment');
-        },
-    });
+    //Meteor.subscribe('userAssignment');
+    //Session.set('userAssignment',[]);
+    //
+    //Template.assignment.helpers({
+    //    'userAssignment':function(){
+    //        Meteor.call('getAssignment',Meteor.userId(),MyApp.currentMonth(), function(error,result){
+    //            Session.set('userAssignment',result);
+    //        });
+    //        return Session.get('userAssignment');
+    //    },
+    //});
 
 }
 
 if (Meteor.isServer) {
-    Meteor.publish('userAssignment',function(){
-        return PrzydzialyCollection.find({});
-
-    });
-
-    Meteor.methods({
-        'getAssignment':function(user,monthNum){
-            var a = new Object;
-            a.month = monthNum;
-            a.userId = user
-            return  PrzydzialyCollection.findOne(a);
-        }
-    });
+    //Meteor.publish('userAssignment',function(){
+    //    return PrzydzialyCollection.find({});
+    //
+    //});
+    //
+    //Meteor.methods({
+    //    'getAssignment':function(user,monthNum){
+    //        var a = new Object;
+    //        a.month = monthNum;
+    //        a.userId = user
+    //        return  PrzydzialyCollection.findOne(a);
+    //    }
+    //});
 
 }
