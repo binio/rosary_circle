4327ee22c8f9979201986a29eea3eb188c885a74
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2016-04-18 23:19:43 +0100

    add enrollment route

--- router/router.js
+++ router/router.js
@@ -5,6 +5,7 @@ Router.route('/most', { controller: 'MostController' });
 Router.route('/users', { controller: 'UsersController' });
 Router.route('/bless', { controller: 'BlessController' });
 Router.route('/assignments',{controller:'AssignmentController'});
+Router.route('/enrollments',{controller:'EnrollmentController'});
 Router.route('/user/:_id', function(){
     profile = UsersCollection.findOne({_id: this.params._id});
     this.render('userDetail', {
