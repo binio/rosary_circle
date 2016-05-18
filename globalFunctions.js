0d7ee49518962286d0a31a87be60f7111dc17a32
binio <tbiniecki@gmail.com>
2016-05-18 11:46:53 +0100

    reorder Rosary

--- globalFunctions.js
+++ globalFunctions.js
@@ -23,25 +23,26 @@ MyApp = (function(){
     api.getPartsArray = function(){
         return [
             'Radosna - Zwiastowanie NMP + wstęp',
-            'Radosna - Nawiedzenie Elżbiety',
+            'Radosna - Nawiedzenie świętej Elżbiety',
             'Radosna - Narodziny Pana Jezusa',
-            'Radosna - Ofiarowanie Pana Jezusa',
-            'Radosna - Odnalezienie Pana Jezusa w Świątyni',
-            'Światła - Chrzest Pana Jezusa w Jordanie',
-            'Światła - Pierwszy cud w Kanie Galilejskiej',
-            'Światła - Głoszenie nauk o Królestwie Bożym',
-            'Światła - Przemienienie na górze Tabor',
-            'Chwalebna - Zmartwychwstanie Pana Jezusa',
-            'Chwalebna - Wniebowstąpienie Pana Jezusa',
-            'Chwalebna - Wniebowstąpienie Najświętszej Marii Panny',
-            'Chwalebna - Ukoronowanie Marii na Królową nieba i Ziemi',
-            'Chwalebna - Zesłanie Ducha Świętego',
+            'Radosna - Ofiarowanie Jezusa w świątyni',
+            'Radosna - Odnalezienie Jezusa w świątyni',
+            'Światła - Chrzest Jezusa w Jordanie',
+            'Światła - Objawienie się Jezusa w Kanie Galilejskiej',
+            'Światła - Głoszenie królestwa i wzywanie do nawrócenia',
+            'Światła - Przemienienie Pańskie na górze Tabor',
             'Światła - Ustanowienie Eucharystii',
             'Bolesna - Modlitwa w Ogrójcu',
             'Bolesna - Biczowanie Pana Jezusa',
-            'Bolesna - Cierniem ukoronowanie',
-            'Bolesna - Dźwiganie krzyża przez Pana Jezusa',
-            'Bolesna - Śmierć Pana Jezusa + Pod Twoją obronę'];
+            'Bolesna - Cierniem ukoronowanie Pana Jezusa',
+            'Bolesna - Dźwiganie krzyża na Kalwarię',
+            'Bolesna - Ukrzyżowanie i śmierć Pana Jezusa',
+            'Chwalebna - Zmartwychwstanie Jezusa Chrystusa',
+            'Chwalebna - Wniebowstąpienie Pana Jezusa',
+            'Chwalebna - Zesłanie Ducha Świętego',
+            'Chwalebna - Wniebowzięcie Najświętszej Maryi Panny',
+            'Chwalebna - Ukoronowanie Maryi na Królową nieba i ziemi + Pod Twoją obronę'
+        ];
     }
 
     return api;
